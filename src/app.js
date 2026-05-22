2ed168bd1668dcffe704c9757d6f91396719b71a
ducnguyen1707 <ducngitech@gmail.com>
2026-05-22 23:57:32 +0700

    adding helmet repo for logging

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+
 const app = express();
 
+app.use(helmet())
 app.get('/', (req, res) => {
+  logger.info('Welcome to DucNguyen Portfolio');
   res.status(200).send('Hi');
 });
 
