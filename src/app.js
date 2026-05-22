849b2a2241c4770982ff3adeb47a65b0ebad0573
ducnguyen1707 <ducngitech@gmail.com>
2026-05-23 00:05:38 +0700

    add morgan for http log

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import logger from './config/logger.js';
 import helmet from 'helmet';
+import morgan from 'morgan';
+
 
 const app = express();
 
 app.use(helmet())
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
+app.use(morgan('combined', {stream: {write: (message) => logger.info(messsage.trim())}}));
+s
 app.get('/', (req, res) => {
   logger.info('Welcome to DucNguyen Portfolio');
   res.status(200).send('Hi');
